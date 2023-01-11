9a9d9b83744a2e2c2c91caf8e7e64ff3651eb35a
root <root@DESKTOP-GUILHERME.localdomain>
2023-01-11 19:41:26 -0300

    Inclusão de classes com state e lifecycle

--- src/components/planets/index.js
+++ src/components/planets/index.js
@@ -5,29 +5,82 @@ import Planet from './planet';
 //   alert(`Planeta clicado ${name}`);
 // }
 
-const Planets = () => {
-  return (
-    <Fragment>
-      <h3>Listagem de Planetas</h3>
-      <button>EVENTO</button>
-      <Planet
-        name='Terra'
-        description='O planeta Terra é o planeta habitado por nós, seres vivos. Conhecido também como planeta água, é o maior dentre os quatro planetas rochosos que fazem parte do Sistema Solar.'
-        img_url='https://s2.static.brasilescola.uol.com.br/be/conteudo/images/planeta-terra.jpg'
-        // clickOnPlanet={clickOnPlanet}
-      />
-      <Planet
-        name='Marte'
-        description='Conforme a União Astronômica Internacional (AIU), órgão responsável pela classificação dos corpos celestes, o sistema solar é composto por oito planetas: Mercúrio, Vênus, Terra, Marte, Júpiter, Saturno, Urano e Netuno. Esses planetas apresentam algumas características em comum, no entanto, cada um possui peculiaridades.'
-        img_url='https://static.mundoeducacao.uol.com.br/mundoeducacao/conteudo_legenda/8465a67d00eda6b73b4485921e5fac7a.jpg'
-        link='https://mundoeducacao.uol.com.br/geografia/planeta-marte.htm'
-        titleWithUnderline = {true}
-        gray={true}
-        // clickOnPlanet={clickOnPlanet}
-      />
-              
-    </Fragment>
-  )
+async function getPlanets(){
+  let response = await fetch('http://localhost:3000/api/planets.json');
+  let data = await response.json();
+  return data;
 }
 
+class Planets extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      planets: []
+    }
+  }
+
+  componentDidMount(){
+    getPlanets().then(data => {
+      this.setState(state => ({
+        planets: data['planets']
+      }))
+    })
+  }
+  
+  removeLast = () => {
+    let newPlanets = [...this.state.planets];
+    newPlanets.pop();
+    this.setState(state => ({
+      planets: newPlanets
+    }))
+  }
+
+  duplicateLast = () => {
+    let lastPlanet = this.state.planets[this.state.planets.length - 1];
+    this.setState(state => ({
+      planets: [...this.state.planets, lastPlanet]
+    }))
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h3>Listagem de Planetas</h3>
+        <button onClick={this.removeLast}>Remover</button>
+        <button onClick={this.duplicateLast}>Duplicar</button>
+        {this.state.planets.map((planet, index) => 
+          <Planet
+            id={planet.id}
+            name={planet.name}
+            description={planet.description}
+            img_url={planet.img_url}
+            link={planet.link}
+            key={index}
+          />
+        )}
+        {/* <Planet
+          // name='Terra'
+          // description='O planeta Terra é o planeta habitado por nós, seres vivos. Conhecido também como planeta água, é o maior dentre os quatro planetas rochosos que fazem parte do Sistema Solar.'
+          // img_url='https://s2.static.brasilescola.uol.com.br/be/conteudo/images/planeta-terra.jpg'
+          // // clickOnPlanet={clickOnPlanet}
+        />
+        <Planet
+          // name='Marte'
+          // description='Conforme a União Astronômica Internacional (AIU), órgão responsável pela classificação dos corpos celestes, o sistema solar é composto por oito planetas: Mercúrio, Vênus, Terra, Marte, Júpiter, Saturno, Urano e Netuno. Esses planetas apresentam algumas características em comum, no entanto, cada um possui peculiaridades.'
+          // img_url='https://static.mundoeducacao.uol.com.br/mundoeducacao/conteudo_legenda/8465a67d00eda6b73b4485921e5fac7a.jpg'
+          // link='https://mundoeducacao.uol.com.br/geografia/planeta-marte.htm'
+          // titleWithUnderline = {true}
+          // gray={true}
+          // // clickOnPlanet={clickOnPlanet}
+        /> */}
+      </Fragment>
+    )
+  }
+}
+
+/*
+const Planets = () => {
+  
+}
+*/
 export default Planets;
--- src/components/planets/planet/index.js
+++ src/components/planets/planet/index.js
@@ -2,22 +2,54 @@ import React, {Fragment} from "react";
 import GrayImg from "../../shared/gray-img";
 import DescriptionWithLink from "../../shared/description-with-link";
 
-const Planet = (props) => {
-  let title;
-  if(props.titleWithUnderline){
-    title = <h4><u>{props.name}</u></h4>
-  } else {
-    title = <h4>{props.name}</h4>
+async function getSatellites(id){
+  let response = await fetch(`http://localhost:3000/api/${id}.json`);
+  let data = await response.json();
+  return data;
+}
+
+class Planet extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      satellites: []
+    }
   }
-  
-  return (
-    //passando como função, ela será executada depois do click
-    <div /*onClick={() => props.clickOnPlanet(props.name)}*/> 
-      {title}
-      <DescriptionWithLink description={props.description} link={props.link}/>
-      <GrayImg img_url={props.img_url} gray={props.gray}/>
-    </div>
-  )
+
+  componentDidMount(){
+    getSatellites(this.props.id).then(data => {
+      this.setState(state => ({
+        satellites: data['satellites']
+      }))
+    }
+    )
+  }
+
+  render(){
+    let title;
+    if(this.props.titleWithUnderline){
+      title = <h4><u>{this.props.name}</u></h4>
+    } else {
+      title = <h4>{this.props.name}</h4>
+    }
+    
+    return (
+      //passando como função, ela será executada depois do click
+      <div /*onClick={() => this.props.clickOnPlanet(this.props.name)}*/> 
+        {title}
+        <DescriptionWithLink description={this.props.description} link={this.props.link}/>
+        <GrayImg img_url={this.props.img_url} gray={this.props.gray}/>
+        <h4>Satélites</h4>
+        <ul>
+          {this.state.satellites.map((satellite, index) =>
+          <li key={index}>{satellite.name}</li>)}
+        </ul>
+      <hr/>
+      </div>
+    )
+  }
+
 }
 
 export default Planet;
